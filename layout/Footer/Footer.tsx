d97cc80280d2b5e44be66178228c0b10551acbf6
DieWerkself <redead29@gmail.com>
2022-09-18 21:16:03 +0300

    Layout app

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,7 +1,20 @@
 import { FooterProps } from "./Footer.props";
 import cn from "classnames";
+import { format } from "date-fns";
 import s from "./Footer.module.css";
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer {...props} className={cn(className, s.footer)}>
+      <div className={s.left}>
+        OwlTop © 2020 - {format(new Date(), "yyyy")} Все права защищены
+      </div>
+      <a href="#" target="_blank">
+        Пользовательское соглашение
+      </a>
+      <a href="#" target="_blank">
+        Политика конфиденциальности
+      </a>
+    </footer>
+  );
 };
